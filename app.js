c50f07145b3e0e6cae07b257659e8a99e537e4da
Jo <a0750318@gmail.com>
2021-09-14 00:36:01 +0800

    add mongoose

--- app.js
+++ app.js
@@ -2,6 +2,23 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const mongoose = require('mongoose') // 載入 mongoose
+mongoose.connect('mongodb://localhost/todo-list') // 設定連線到 mongoDB
+//取得資料庫連線狀態
+const db = mongoose.connection
+//連線異常
+db.on('error', () => {
+    console.log('mongodb error!')
+})
+//連線成功
+db.once('open', () => {
+    console.log('mongodb connected!')
+})
+
 app.get('/', (req, res) => {
     console.log('todo list')
+})
+
+app.listen(3000,() => {
+    console.log('App is running on http://localhost:3000.')
 })
