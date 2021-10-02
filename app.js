ad187e33ae03ada67d7b139eb496102bcc26dd43
Jo <a0750318@gmail.com>
2021-10-02 19:06:35 +0800

    modify express routes

--- app.js
+++ app.js
@@ -4,9 +4,16 @@ const exphbs = require('express-handlebars');
 const Todo = require('./models/todo') // 載入 Todo model
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const router = express.Router()
+// 引用路由器
+const routes = require('./routes')
+
 
 const app = express()
 const port = 3000
+
+
+
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
@@ -28,62 +35,9 @@ db.once('open', () => {
 //轉換PUT跟DELETE
 app.use(methodOverride('_method'))
 
-app.get('/', (req, res) => {
-    Todo.find() // 取出 Todo model 裡的所有資料
-    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
-    .sort({_id: 'asc'})
-    .then(todos => res.render('index', { todos })) // 將資料傳給 index 樣板
-    .catch(error => console.error(error)) // 錯誤處理
-})
+// 將 request 導入路由器
+app.use(routes)
 
-app.get('/todos/new', (req, res) => {
-    return res.render('new')
-  })
-
-  app.post('/todos', (req, res) => {
-    const name = req.body.name       // 從 req.body 拿出表單裡的 name 資料
-    return Todo.create({ name })     // 存入資料庫
-      .then(() => res.redirect('/')) // 新增完成後導回首頁
-      .catch(error => console.log(error))
-  })
-
-  app.get('/todos/:id', (req, res) => {
-    const id = req.params.id
-    return Todo.findById(id)
-      .lean()
-      .then((todo) => res.render('detail', { todo }))
-      .catch(error => console.log(error))
-  })
-
-  app.get('/todos/:id/edit', (req, res) => {
-    const id = req.params.id
-    return Todo.findById(id)
-      .lean()
-      .then((todo) => res.render('edit', { todo }))
-      .catch(error => console.log(error))
-  })
-
-  app.post('/todos/:id', (req, res) => {
-    const id = req.params.id
-    // const name = req.body.name
-    const { name, isDone } = req.body //解構賦值
-    return Todo.findById(id)
-      .then(todo => {
-        todo.name = name
-        todo.isDone = isDone === 'on'
-        return todo.save()
-      })
-      .then(()=> res.redirect(`/todos/${id}`))
-      .catch(error => console.log(error))
-  })
-
-  app.post('/todos/:id', (req, res) => {
-    const id = req.params.id
-    return Todo.findById(id)
-      .then(todo => todo.remove())
-      .then(() => res.redirect('/'))
-      .catch(error => console.log(error))
-  })
 
 app.listen(3000,() => {
     console.log('App is running on http://localhost:3000.')
---
+++ routes/modules/todos.js
@@ -0,0 +1,54 @@
+// 引用 Express 與 Express 路由器
+const express = require('express')
+const router = express.Router()
+
+const Todo = require('../../models/todo')
+
+router.post('/', (req, res) => {
+      const name = req.body.name       // 從 req.body 拿出表單裡的 name 資料
+      return Todo.create({ name })     // 存入資料庫
+      .then(() => res.redirect('/')) // 新增完成後導回首頁
+      .catch(error => console.log(error))
+    })
+    
+    router.get('/new', (req, res) => {
+        return res.render('new')
+      })
+      router.get('/:id', (req, res) => {
+    const id = req.params.id
+    return Todo.findById(id)
+      .lean()
+      .then((todo) => res.render('detail', { todo }))
+      .catch(error => console.log(error))
+  })
+
+  router.get('/:id/edit', (req, res) => {
+    const id = req.params.id
+    return Todo.findById(id)
+      .lean()
+      .then((todo) => res.render('edit', { todo }))
+      .catch(error => console.log(error))
+  })
+
+  router.put('/:id', (req, res) => {
+    const id = req.params.id
+    // const name = req.body.name
+    const { name, isDone } = req.body //解構賦值
+    return Todo.findById(id)
+      .then(todo => {
+        todo.name = name
+        todo.isDone = isDone === 'on'
+        return todo.save()
+      })
+      .then(()=> res.redirect(`/todos/${id}`))
+      .catch(error => console.log(error))
+  })
+
+  router.delete('/:id', (req, res) => {
+    const id = req.params.id
+    return Todo.findById(id)
+      .then(todo => todo.remove())
+      .then(() => res.redirect('/'))
+      .catch(error => console.log(error))
+  })
+  module.exports = router
